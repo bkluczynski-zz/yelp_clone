ea8d5506a614ead5b9fa3f7ca034083553a4640f
Bart <kluczynskib@gmail.com>
2016-08-22 22:16:41 +0200

    finished section 8-th

--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 get 'restaurants' => 'restaurants#index'
-get 'new_restaurant' => 'restaurants#new'
+# get 'new_restaurant' => 'restaurants#new'
 
 resources :restaurants
   # The priority is based upon order of creation: first created -> highest priority.
