3294e9a8ce38f36f377d3ba3ec4f0e3b2e27e657
Luke Cartledge <luke@lukecartledge.com>
2016-08-25 14:18:31 +0100

    more styling and layout I guess - still not happy with links in the nav bar

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe User, type: :model do
-  it { have_many(:restaurant) }
-  it { have_many(:review) }
+  it { should have_many(:restaurants) }
+  it { should have_many(:reviews) }
+  # it { should have_many(:reviewed_restaurants) }
+  it { is_expected.to have_many :reviewed_restaurants}
 end
