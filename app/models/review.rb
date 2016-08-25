967deb52feece4ad37c7fab04de3db7a826966c4
Luke Cartledge <luke@lukecartledge.com>
2016-08-25 15:14:46 +0100

    completes restaurant - review association

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,12 @@
 class Review < ActiveRecord::Base
 
   belongs_to :user
+  belongs_to :restaurant
   validates :rating, inclusion: (1..5)
+  validates :user, uniqueness: { scope: :restaurant, message: "You have reviewed this restaurant already" }
+
+  def build_review
+
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,5 @@ describe User, type: :model do
   it { should have_many(:restaurants) }
   it { should have_many(:reviews) }
   # it { should have_many(:reviewed_restaurants) }
-  it { is_expected.to have_many :reviewed_restaurants}
+  # it { is_expected.to have_many :reviewed_restaurants}
 end
